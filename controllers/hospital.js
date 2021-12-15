c91eb16fa6a24209e283d14eecbbe698e3c11387
fady gamil mahrous masoud <1700961@eng.asu.edu.eg>
2021-12-15 02:17:32 +0200

    get-doctors-of-specific-hospital API is finished

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -1,5 +1,7 @@
 // import Hospital model
 const { Hospital } = require("../models/Hospital");
+// import Docotr model
+const { Doctor } = require("../models/Doctor");
 
 // @METHOD                                             POST
 // @URL                                                   /hospital
@@ -11,6 +13,26 @@ exports.createNewHospital = async (req, res, next) => {
 		const hospital = await Hospital.create(req.body);
 		// (2) return the new created document to client in response
 		return res.status(201).json(hospital);
+	} catch (error) {
+		console.log(error.message);
+		next(error);
+	}
+};
+
+// @METHOD                                             GET
+// @URL                                                   /hospital/:hospitalID
+// @ACTION_PERFORMEER                          user of application (mainly the patient)
+// @DESCRIPTION                                      user need to see all doctors of this hospital
+exports.getAllDoctors = async (req, res, next) => {
+	try {
+		// (1) get the hospital id
+		const { hospitalID } = req.params;
+		// (2) fetch all doctors that thier entity_id field === hospitalID
+		const doctors = await Doctor.find({
+			entity_id: hospitalID,
+		});
+		// (3) return response to client
+		return res.status(200).json(doctors);
 	} catch (error) {
 		console.log(error.message);
 		next(error);
--- routes/hospital.js
+++ routes/hospital.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 // import the route handler "controller"
-const { createNewHospital } = require("../controllers/hospital");
+const { createNewHospital, getAllDoctors } = require("../controllers/hospital");
 
 // chain the route to the router app to chain it to main express app
 router.route("/hospital").post(createNewHospital);
+
+router.route("/hospital/:hospitalID").get(getAllDoctors);
 
 module.exports = router;
