109763137cae638e9c12bf5afa697c92ba995289
maryWkhairy <mary98waseem@gmail.com>
2022-05-02 14:54:19 +0200

    edits

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
-const { getDoctorsOfEntity, setTimeTable } = require("../controllers/doctor");
+const { getDoctorsOfEntity, setTimeTable,editDoctorInfo,
+    editDoctorPhoto } = require("../controllers/doctor");
 const { protect } = require("../middleware/authMiddleware");
 //router.route("/doctor/rating").patch(rateDoctor);//rate doctor
 //router.route("/user/rating/doctor/doctorname").patch(rateDoctor);//rate doctor mogoda f el user routes
@@ -9,5 +10,7 @@ const { protect } = require("../middleware/authMiddleware");
 router.route("/entity/:entity/doctors").get(getDoctorsOfEntity);// doctors in entity //2 applications
 //router.route("/eli by3ml el action/el resource eli byt3ml 3aleh el action").get(getDoctorsOfEntity)// doctors in entity
 router.route("/doctor/timetable").patch(protect,setTimeTable);
+router.route("/doctor/edit/info").patch(protect,editDoctorInfo);
+router.route("/doctor/edit/photo").patch(protect,editDoctorPhoto);
 
 module.exports = router;
